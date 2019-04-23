9bdc687de6335ebed9bbb8ff9a3bf1db0fa6eb11
yogendra <datanapps@gmail.com>
2019-04-23 12:10:32 +0800

    convert this into kotlin

---
+++ app/src/main/java/datanapps/roomapi/Book.kt
@@ -0,0 +1,8 @@
+package datanapps.roomapi
+
+class Book {
+    var bookTitle: String? = null
+    var authorName: String? = null
+    var bookImage: String? = null
+    var publishedYear: Int = 0
+}
